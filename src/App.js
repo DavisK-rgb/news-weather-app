aa5c1ee78286e82423181967aa638b974bd8c14a
Davis <katendedavis93@gmail.com>
2022-04-06 03:10:26 +0300

    draft 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { NewsList } from './components'
-
+import Iframe from 'react-iframe'
 function App() {
   return (
     <div className="App">
+
       <NewsList />
     </div>
   );
